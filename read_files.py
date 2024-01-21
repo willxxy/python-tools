4d60f35a73e8e94a4ec67df9cb510716b2635a6a
willxxy <90741489+willxxy@users.noreply.github.com>
2024-01-20 20:34:25 -0500

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -3,3 +3,7 @@ file_path = '/path/to/txt_file.txt'
 
 with open(file_path, 'r') as file:
     file_contents = file.readlines()
+
+### JSON STUFF
+with open('your_file.json', 'r') as file:
+    data = json.load(file)
