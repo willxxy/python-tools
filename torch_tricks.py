bb805cc7090eab952dd91628af982a6dce365cd1
willxxy <90741489+willxxy@users.noreply.github.com>
2024-02-28 14:47:15 -0500

    Update torch_tricks.py

--- torch_tricks.py
+++ torch_tricks.py
@@ -1,3 +1,18 @@
 ### set number of CPU threads
 import torch
 torch.set_num_threads(2) # 4 is also good
+
+
+### Seeding
+# This still produces random x
+torch.manual_seed(0)
+for i in range(10):
+    x = torch.rand(5, 3)
+    print(x)
+  
+# This produces same x
+for i in range(10):
+    torch.manual_seed(0)
+    x = torch.rand(5, 3)
+    print(x)
+
