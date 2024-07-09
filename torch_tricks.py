6ea63cf98f42d911f8b459a4434e7e13541601c6
willxxy <90741489+willxxy@users.noreply.github.com>
2024-07-09 11:00:48 -0400

    Update torch_tricks.py

--- torch_tricks.py
+++ torch_tricks.py
@@ -19,3 +19,9 @@ for i in range(10):
 # counts the number of parameters in a model
 def count_parameters(model: nn.Module) -> int:
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+# print the parameters if they need gradient
+def print_gradients(module):
+    for name, param in module.named_parameters():
+        if param.requires_grad:
+            param.register_hook(lambda grad, name=name: print(f'Gradient for {name}: {grad is not None}'))
